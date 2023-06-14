65d47fe33ad1e28f8332546902a337ae7a8f3fb1
Erfan Momeni <Erfan.Momeni@snapp.cab>
2023-06-14 15:52:24 +0330

    test: adding benchmark for compare with list

---
+++ benchmarks/scenario_bench_test.go
@@ -0,0 +1,51 @@
+package benchmarks
+
+import (
+	"container/list"
+	"guthub.com/erfanmomeniii/queue"
+	"testing"
+)
+
+func BenchmarkCompare(b *testing.B) {
+	b.Logf("Adding an element to the queue")
+	b.Run("container/list", func(b *testing.B) {
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				l := list.New()
+				l.PushFront("hi")
+			}
+		})
+	})
+	b.Run("erfanmomeniii/queue", func(b *testing.B) {
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				q := queue.New()
+				q.PushFront("hi")
+			}
+		})
+	})
+
+	b.Logf("Removing an element from the queue")
+	b.Run("container/list", func(b *testing.B) {
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				l := list.New()
+				l.PushFront(4)
+				l.Remove(l.Front())
+			}
+		})
+	})
+	b.Run("erfanmomeniii/queue", func(b *testing.B) {
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				q := queue.New()
+				q.PushFront(4)
+				q.PopFront()
+			}
+		})
+	})
+}
